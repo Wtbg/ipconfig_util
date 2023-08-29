5706d72b3252ba2f8197b61742f0a15223c1a91d
Wtbg <3210101820@zju.edu.cn>
2023-08-29 10:26:30 +0800

    finish basic function

--- provide_ipconfig/src/main.rs
+++ provide_ipconfig/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use lazy_static::lazy_static;
+use log::{error, info, Level};
+use std::sync::Mutex;
+use std::{env, fs::File, io::Write, path::PathBuf, process::Command};
+lazy_static! {
+    pub static ref IPCONFIG_OUTPUT: Mutex<String> = Mutex::new(String::new());
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "error");
+    env_logger::init();
+    HttpServer::new(|| App::new().service(ipconfig).service(get_ipconfig))
+        .bind("0.0.0.0:7777")?
+        .run()
+        .await
+}
+
+//accept string as request body, save it in a local file ipconfig.txt
+#[post("/ipconfig")]
+async fn ipconfig(req_body: String) -> impl Responder {
+    let mut file = std::fs::File::create("ipconfig.txt").unwrap();
+    let mut ipconfig_output = IPCONFIG_OUTPUT.lock().unwrap();
+    *ipconfig_output = req_body.clone();
+    match file.write_all(req_body.as_bytes()) {
+        Ok(_) => info!("write to file success"),
+        Err(e) => error!("write to file failed: {}", e),
+    }
+    HttpResponse::Ok().body("ok")
+}
+
+#[get("/ipconfig")]
+async fn get_ipconfig() -> impl Responder {
+    let ipconfig_output = IPCONFIG_OUTPUT.lock().unwrap();
+    info!("ipconfig: {}", ipconfig_output.clone());
+    HttpResponse::Ok().body(ipconfig_output.clone())
 }
--- send_ipconfig/src/main.rs
+++ send_ipconfig/src/main.rs
@@ -8,18 +8,22 @@ use env_logger::*;
 use log::{error, info, Level};
 use std::env;
 use reqwest::Client;
+use dotenv::dotenv;
+use encoding_rs::{UTF_8, GBK};
 
 #[actix_web::main]
 async fn main() {
+    dotenv().ok();
     env::set_var("RUST_LOG", "info");
     env_logger::init();
     let host_ip = env::var("HOST_IP").expect("HOST_IP not set");
-    let client = Client::new();
-    let body = get_ipconfig().await;
-    let res = client.post(format!("http://{}/ipconfig", host_ip))
-        .body(body)
-        .send()
-        .await;
+    info!("HOST_IP: {}", host_ip);
+    let host_port = env::var("HOST_PORT").expect("HOST_PORT not set");
+    info!("HOST_PORT: {}", host_port);
+    loop{
+        send_ipconfig(host_ip.clone(), host_port.clone()).await;
+        std::thread::sleep(std::time::Duration::from_secs(10));
+    }
 }
 
 async fn get_ipconfig() -> String {
@@ -27,5 +31,25 @@ async fn get_ipconfig() -> String {
         .arg("/all")
         .output()
         .expect("failed to execute process");
-    String::from_utf8(output.stdout).unwrap()
+    match output.status.success(){
+        true => {
+            let (result, _, _) = GBK.decode(&output.stdout);
+            // info!("ipconfig: {}", result.to_string());
+            result.to_string()
+        },
+        false => {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            stderr.to_string()
+        },
+    }
+}
+
+async fn send_ipconfig(host_ip: String, host_port: String){
+    let client = Client::new();
+    let body = get_ipconfig().await;
+    let res = client.post(format!("http://{}:{}/ipconfig", host_ip, host_port))
+        .body(body)
+        .send()
+        .await;
+    info!("send ipconfig to server: {:?}", res);
 }
